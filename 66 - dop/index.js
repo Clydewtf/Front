8d6491c1c062905376209e3f62e68c8a2f9081fa
Yuuki <104145158+Clydewtf@users.noreply.github.com>
2024-01-19 22:58:45 +0700

    Add files via upload

--- 66 - dop/index.js
+++ 66 - dop/index.js
@@ -1,32 +1,55 @@
-import { fetchProducts, fetchCategories, fetchProductsByCategory, fetchSearchByProducts } from "./requests.js";
-import { addProductToContainer } from "./utils.js";
+import {getCategories, getProducts, getProductsByCategory, getProductsBySearch} from "./requests.js";
 
-const productsContainer = document.querySelector('.js-products');
-const select = document.querySelector('.js-select');
-const search = document.querySelector('.js-search');
+const products = document.querySelector('.js-products')
+const select = document.querySelector('.js-select')
+const search = document.querySelector('.js-search')
 
-// Обработчик выбора категории
+const formatter = Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+})
+
+getProducts().then((data) => {
+    printProducts(data.products)
+})
+
+getCategories().then(data => {
+    data.forEach((item) => {
+        const option = `<option value="${item}">${item}</option>`
+        select.insertAdjacentHTML('beforeend', option)
+    })
+})
+
 select.addEventListener('change', () => {
-    const selectedCategory = select.value;
-    if (selectedCategory) {
-        fetchProductsByCategory(selectedCategory);
-    } else {
-        // Если выбрана опция "Выберите категорию", отображаем все товары
-        fetchProducts();
-    }
-});
+    getProductsByCategory(select.value).then((data) => {
+        printProducts(data.products)
+    })
+})
 
-// Обработчик поиска
 search.addEventListener('input', () => {
-    const searchWord = search.value.trim();
-    if (searchWord) {
-        fetchSearchByProducts(searchWord);
-    } else {
-        // Если поле поиска пустое, отображаем все товары
-        fetchProducts();
-    }
-});
+    select.value = ''
+    getProductsBySearch(search.value).then(data => {
+        printProducts(data.products)
+    })
+})
 
-// Загружаем все категории и товары при загрузке страницы
-fetchCategories();
-fetchProducts();
+function printProducts(data) {
+    products.innerHTML = ''
+    data.forEach((item) => {
+        const product = `<div class="product">
+                <img class="product__img" src="${item.thumbnail}" alt="">
+                <div class="product__body">
+                    <p class="product__price">${formatter.format(item.price)}</p>
+                    <h2 class="product__title">${item.title}</h2>
+                    <p class="product__descr">${item.description}</p>
+                    <p class="product__brand">Brand: <span>${item.brand}</span></p>
+                    <p class="product__category">Category: <span>${item.category}</span></p>
+                    <div class="rating product__rating">
+                        <svg class="rating__ico" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg>
+                        <p class="rating__value">${item.rating}</p>
+                    </div>
+                </div>
+            </div>`
+        products.insertAdjacentHTML('beforeend', product)
+    })
+}
--- 66 - dop/requests.js
+++ 66 - dop/requests.js
@@ -1,62 +1,23 @@
-export function fetchProducts() {
-    const url = 'https://dummyjson.com/products/';
-    return fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-            const productsArray = Array.isArray(data.products) ? data.products : [data.products];
-            productsArray.forEach(addProductToContainer);
-        })
-        .catch((error) => {
-            console.error('Error fetching products:', error);
-        });
+export function getProducts() {
+    return fetch("https://dummyjson.com/products")
+        .then(response => response.json())
+        .catch(console.error)
 }
 
-export function fetchCategories() {
-    const url = 'https://dummyjson.com/products/categories';
-    return fetch(url)
-        .then((response) => response.json())
-        .then((categories) => {
-            categories.forEach((category) => {
-                const option = document.createElement('option');
-                option.value = category;
-                option.textContent = category;
-                select.appendChild(option);
-            });
-        })
-        .catch((error) => {
-            console.error('Error fetching categories:', error);
-            return []; // Возвращаем пустой массив в случае ошибки
-        });
+export function getProductsByCategory(category) {
+    return fetch(`https://dummyjson.com/products/category/${category}`)
+        .then(response => response.json())
+        .catch(console.error)
 }
 
-export function fetchProductsByCategory(category) {
-    const url = `https://dummyjson.com/products/category/${category}`;
-    return fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-            const productsArray = Array.isArray(data.products) ? data.products : [data.products];
-
-            // Очищаем текущий контейнер и добавляем новые товары
-            productsContainer.innerHTML = '';
-            productsArray.forEach(addProductToContainer);
-        })
-        .catch((error) => {
-            console.error('Error fetching products by category:', error);
-        });
+export function getCategories() {
+    return fetch('https://dummyjson.com/products/categories')
+        .then(response => response.json())
+        .catch(console.error)
 }
 
-export function fetchSearchByProducts(query) {
-    const url = `https://dummyjson.com/products/search?q=${query}`;
-    return fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-            const productsArray = Array.isArray(data.products) ? data.products : [data.products];
-
-            // Очищаем текущий контейнер и добавляем новые товары
-            productsContainer.innerHTML = '';
-            productsArray.forEach(addProductToContainer);
-        })
-        .catch((error) => {
-            console.error('Error searching for products:', error);
-        });
+export function getProductsBySearch(query) {
+    return fetch("https://dummyjson.com/products/search?q=" + query)
+        .then(response => response.json())
+        .catch(console.error)
 }
